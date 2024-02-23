7fe477cb00644aa309b1ce4b8e43f93a80cf7970
matheus.hodekcer <matheus.hodecker.santos@gmail.com>
2024-02-23 15:07:16 -0300

    contador

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,42 @@
 <script setup>
 import { ref } from 'vue';
 
-const contador = ref(0)
+const contador = ref(0);
 function decrementar(){
-  contador.value--
-}
-function acrecentar(){
-  contador.value++
-}
+  if(contador.value > 0){
 
+  contador.value--;
+}}
+
+
+function acrecentar(){
+
+    contador.value++;
+  
+}
+function reiniciar(){
+
+  contador.value = 0;
+
+}
 
 </script>
 
 <template>
   <button @click="acrecentar">+</button>
   <button @click="decrementar">-</button>
+  <button @click="reiniciar"></button>
   <p>valor do contador: {{ contador }}</p>
+ 
 </template>
 
-<style scoped></style>
+<style scoped>
+button {
+  background-color: chocolate;
+  color: white;
+  font-weight: bolder;
+  border: 0;
+  margin: 2px;
+  padding: 20px;
+}
+</style>
